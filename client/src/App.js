6aecd1ee1bde83a8a83f2887f7b8ad2d7d1baa14
Pajkic-lab <markopajkic@ymail.com>
2021-02-25 17:09:49 +0100

    change

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Radi
+          Works !!!!!
         </p>
       </header>
     </div>
