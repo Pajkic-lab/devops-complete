613c8279d835abb8fb91f70f2544765bbe4cc1d5
Pajkic-lab <markopajkic@ymail.com>
2021-02-25 19:08:54 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Marko Pajkic
+          milos pajkic
         </p>
       </header>
     </div>
